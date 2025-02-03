7502741ad65ed052caf301aa96ad5a9525d1b5b6
mtsnowden <mtsnowden@gmail.com>
2025-02-02 19:21:12 -0600

    added navbar and searchbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import UserLayout from './components/Layout/UserLayout'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<UserLayout />}></Route>
+      <Route>{/* Admin Layout */}</Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
