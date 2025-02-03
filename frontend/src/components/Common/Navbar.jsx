5ebd456d785843ddf64026bdfc61e851a64d90e3
mtsnowden <mtsnowden@gmail.com>
2025-02-02 21:06:42 -0600

    added hamburger menu toggle drawer

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -3,11 +3,17 @@ import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight } from "react-i
 import SearchBar from './SearchBar'
 import CardDrawer from '../Layout/CardDrawer'
 import { useState } from 'react'
+import { IoMdClose } from 'react-icons/io'
 
 
 const Navbar = () => {
 
     const [drawerOpen, setDrawerOpen] = useState(false);
+    const [navDrawerOpen, setNavDrawerOpen] = useState(false);
+
+    const toggleNavDrawer = () => {
+        setNavDrawerOpen(!navDrawerOpen)
+    }
 
     const toggleCartDrawer = () => {
         setDrawerOpen(!drawerOpen);
@@ -52,12 +58,22 @@ const Navbar = () => {
         </div>
         
 
-        <button className='md:hidden'>
+        <button onClick={toggleNavDrawer} className='md:hidden'>
             <HiBars3BottomRight className='h-6 w-6 text-gray-700' />
         </button>
         </div>
     </nav>
     <CardDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
+    
+    {/* mobile navigation */}
+
+    <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${navDrawerOpen ? "translate-x-0" : "-translate-x-full"}`}>
+        <div className="flex justify-end p-4">
+            <button onClick={toggleNavDrawer}>
+                <IoMdClose className='h-6 w-6 text-gray-600' />
+            </button>
+        </div>
+    </div>
     </>
   )
 }
