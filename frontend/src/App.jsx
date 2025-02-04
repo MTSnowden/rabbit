6458383532a180266d88d6ebbe69f4db5e499658
mtsnowden <mtsnowden@gmail.com>
2025-02-04 10:22:12 -0600

    added hero section. completed gender section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import UserLayout from './components/Layout/UserLayout'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<UserLayout />}></Route>
+      <Route path="/" element={<UserLayout />}>
+         <Route index element={<Home />} />
+      </Route>
+      
       <Route>{/* Admin Layout */}</Route>
     </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../components/Layout/Hero'
+import GenderCollectionSection from '../components/Products/GenderCollectionSection'
+
+const Home = () => {
+  return (
+    <div>
+        <Hero />
+        <GenderCollectionSection />
+    </div>
+   
+  )
+}
+
+export default Home
